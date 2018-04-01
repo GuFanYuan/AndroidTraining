3c077e3c864b7bf9d74a9f4226b230543205fbf2
GuFanYuan <947157548@qq.com>
2018-04-02 00:29:04 +0800

    for login activity

---
+++ ITourism/app/src/main/java/com/example/gufan/itourism/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.gufan.itourism;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by gufan on 2018/4/1.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- ITourism/app/src/main/java/com/example/gufan/itourism/fragment/MeFragment.java
+++ ITourism/app/src/main/java/com/example/gufan/itourism/fragment/MeFragment.java
@@ -1,13 +1,18 @@
 package com.example.gufan.itourism.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.gufan.itourism.LoginActivity;
 import com.example.gufan.itourism.R;
+
+import java.io.BufferedInputStream;
 
 /**
  * Created by gufan on 2018/4/1.
@@ -15,9 +20,23 @@ import com.example.gufan.itourism.R;
 
 public class MeFragment extends Fragment{
 
+    private Button mBtnLogin;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mBtnLogin = getView().findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginIntent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
     }
 }
